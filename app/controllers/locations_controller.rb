13caaac618e813574df1ae785bc0ba3d5f28ae37
Don Park <don.park@gmail.com>
2008-09-07 16:18:42 -0700

    save a new record

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,17 @@
 class LocationsController < ApplicationController
+
+  def index
+    @location_count = Location.count
+  end
+
+  def create
+    location = Location.new(:guid => params[:location][:guid],
+                            :geom => Point.from_x_y_z(params[:location][:long],
+                                                      params[:location][:lat],
+                                                      100,123))
+    location.save!
+    flash[:notice] = "new location record saved."
+    redirect_to locations_path
+
+  end
 end
