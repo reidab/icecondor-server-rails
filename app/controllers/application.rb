95d1ee0a27942f4f96694a949d4b5ff701909e7e
Don Park <don.park@gmail.com>
2008-09-10 08:15:27 -0700

    turn off cross site script token

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'e9f400a335857817f3a6191387175b57'
+  #protect_from_forgery # :secret => 'e9f400a335857817f3a6191387175b57'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
