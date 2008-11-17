0fd7aeff9f4cef14e63731ab910b00fefbac20f2
Don Park <don.park@gmail.com>
2008-11-17 12:01:39 -0800

    add a user summary display page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,4 +4,9 @@ class DashboardController < ApplicationController
     @locations = Location.last_updates(@last_users_reporting,3)
   end
 
+  def users
+    @last_users_reporting = Location.last_users_reporting(5)
+    @locations = Location.last_updates(@last_users_reporting,1)
+  end
+
 end
