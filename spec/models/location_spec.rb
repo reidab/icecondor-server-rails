50abc8c79ba602ed605cd3bbe23ee38942812e59
Don Park <don@donpark.org>
2008-12-03 17:22:59 -0800

    test fixup

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -6,11 +6,11 @@ describe Location do
   before(:each) do
     @valid_attributes = {
       :guid => "value for guid",
-      :time => Time.now,
+      :timestamp => Time.now,
       :longitude => -122.5,
       :latitude => 45.5,
       :altitude => 10.0,
-      :timestamp => '2008-10-11T04:05:06'
+      :created_at => '2008-10-11T04:05:06'
     }
   end
 
@@ -19,35 +19,35 @@ describe Location do
   end
 
   it "should require a valid lat/long" do
-    location =  Location.create(:guid => "foo", :time => Time.now)
+    location =  Location.create(:guid => "foo", :timestamp => Time.now)
     location.should have(1).error_on(:geom)
   end
 
   it "should update the latitude" do
     new_latitude = 45 
-    location = locations(:one)
+    location = locations(:fu0)
     location.latitude = new_latitude
     location.latitude.should == new_latitude
   end
 
   it "should update the longitude" do
     new_longitude = 122
-    location = locations(:one)
+    location = locations(:fu0)
     location.latitude = new_longitude
     location.latitude.should == new_longitude
   end
 
   it "should update the altitude" do
     new_altitude = 1000
-    location = locations(:one)
+    location = locations(:fu0)
     location.altitude = new_altitude
     location.altitude.should == new_altitude
   end
 
   it "should update the time" do
-    new_time = '2008-10-12T05:15:00'
-    location = locations(:one)
-    location.timestamp = new_timestamp
-    location.timestamp.should == new_timestamp
+    new_time = Time.parse('2008-10-12T05:15:00')
+    location = locations(:fu0)
+    location.timestamp = new_time
+    location.timestamp.should == new_time
   end
 end
