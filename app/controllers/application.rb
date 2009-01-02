a2cb46ac554a8058ca513f4a578db82a56c0c9b9
Don Park <don@donpark.org>
2009-01-02 13:14:43 -0800

    starting bits for an iPhone-specific HTML view

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,6 +3,8 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  before_filter :adjust_format_for_iphone
+
 
   # Use our standard layout for all non-AJAX/non-RSS-feed requests
   layout proc {|c| (c.request.xhr? || c.request.format.to_s.include?("xml")) ? false : "standard-layout" }
@@ -15,4 +17,14 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  def adjust_format_for_iphone    
+    request.format = Mime::Type.lookup_by_extension("iphone") if iphone_user_agent?
+    logger.info "new format "+request.format.class.name+" "+request.format
+  end
+
+  # Request from an iPhone or iPod touch? (Mobile Safari user agent)
+  def iphone_user_agent?
+    !(request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]).nil?
+  end
 end
