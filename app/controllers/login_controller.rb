46d2ce5e2302962279a06ae94cef705c9495088c
Don Park <don@donpark.org>
2009-01-05 15:31:08 -0800

    OpenID verification process complete

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -11,14 +11,46 @@ class LoginController < ApplicationController
           redirect_to :root
           return
         end
-        idreq = consumer.begin(identifier)
+        oidreq = consumer.begin(identifier)
       rescue OpenID::OpenIDError => e
         flash[:error] = "Discovery failed for #{identifier}: #{e}"
         redirect_to :root
         return
       end
+      return_to = url_for :action => 'complete', :only_path => false
+      realm = url_for :action => 'index', :only_path => false
 
-
+      if oidreq.send_redirect?(realm, return_to, params[:immediate])
+        redirect_to oidreq.redirect_url(realm, return_to, params[:immediate])
+      else
+        render :text => oidreq.html_markup(realm, return_to, params[:immediate], {'id' => 'openid_form'})
+      end
     end
   end
+
+  def complete
+    # FIXME - url_for some action is not necessarily the current URL.
+    current_url = url_for(:action => 'complete', :only_path => false)
+    parameters = params.reject{|k,v|request.path_parameters[k]}
+    oidresp = consumer.complete(parameters, current_url)
+    case oidresp.status
+    when OpenID::Consumer::FAILURE
+      if oidresp.display_identifier
+        flash[:error] = ("Verification of #{oidresp.display_identifier}"\
+                         " failed: #{oidresp.message}")
+      else
+        flash[:error] = "Verification failed: #{oidresp.message}"
+      end
+    when OpenID::Consumer::SUCCESS
+      flash[:success] = ("Verification of #{oidresp.display_identifier}"\
+                         " succeeded.")
+    when OpenID::Consumer::SETUP_NEEDED
+      flash[:alert] = "Immediate request failed - Setup Needed"
+    when OpenID::Consumer::CANCEL
+      flash[:alert] = "OpenID transaction cancelled."
+    else
+    end
+    redirect_to :root
+  end
+
 end
