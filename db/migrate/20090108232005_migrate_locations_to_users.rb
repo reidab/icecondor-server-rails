dc52e965dfdcb314c96a2b561a6613f5fc8d9f62
Don Park <don.park@gmail.com>
2009-01-11 13:06:24 -0800

    gak, fixes for the migration

--- db/migrate/20090108232005_migrate_locations_to_users.rb
+++ db/migrate/20090108232005_migrate_locations_to_users.rb
@@ -2,9 +2,10 @@ class MigrateLocationsToUsers < ActiveRecord::Migration
   def self.up
     add_column :locations, :user_id, :integer
     Location.all.each do |l|
-      user = User.find(:first, :conditions => {:guid => l.guid})
+      user = Openidentity.find(:first, :conditions => {:url => l.guid})
       unless user
-        user = User.create({:guid => l.guid})
+        user = User.create()
+        Openidentity.create({:user => user, :url => l.guid})
       end
       l.update_attribute :user_id, user.id
     end
