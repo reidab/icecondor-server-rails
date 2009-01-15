d8295bd5c347b6eeba173c938ed63422def0215d
Don Park <don@donpark.org>
2009-01-15 11:47:50 -0800

    test the second half of an openid login

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -14,4 +14,17 @@ describe SessionController do
     post :login, {:openid_identifier => openid }
     response.should redirect_to(provider_url)
   end
+
+  it "should complete the authentication of an openid url" do
+    openid = "http://phoneyid"
+    openid_response = mock("openid response")
+    openid_response.should_receive(:status).and_return(OpenID::Consumer::SUCCESS)
+    openid_response.stub!(:display_identifier).and_return(openid)
+    consumer = mock("openid consumer")
+    consumer.should_receive(:complete).and_return(openid_response)
+    controller.should_receive(:consumer).and_return(consumer)
+
+    post :complete
+    controller.current_user.should == openid
+  end
 end
