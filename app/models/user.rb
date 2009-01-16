475f2424b04ad0bb35643db3019fab57ed924298
Don Park <don.park@gmail.com>
2009-01-15 18:42:07 -0800

    of course a user has_many locations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :openidentities
   has_many :client_applications
   has_many :tokens, :class_name=>"OauthToken",:order=>"authorized_at desc",:include=>[:client_application]
+  has_many :locations
 end
