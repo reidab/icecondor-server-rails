5c8ca23a226822b36e73f5ceaa39c2745d7b08a5
Don Park <don@donpark.org>
2009-01-18 18:06:02 -0800

    test the lat/long values for a location update

--- spec/controllers/location_controller_spec.rb
+++ spec/controllers/location_controller_spec.rb
@@ -4,12 +4,16 @@ describe LocationsController do
   it "should process a location update" do
     identity = "http://testperson/"
     count = Location.count
-    record = {"client"=>{"version"=>"20081227"}, "location"=>{"timestamp"=>"2009-01-09T18:43:56+0000", "latitude"=>"45.5118191242218", "guid"=>identity, "altitude"=>"35.0", "accuracy"=>"3072.0", "longitude"=>"-122.63253271579742"}}
+    latitude = 45.5118191242218
+    longitude = -122.63253271579742
+    record = {"client"=>{"version"=>"20081227"}, "location"=>{"timestamp"=>"2009-01-09T18:43:56+0000", "latitude"=>latitude, "guid"=>identity, "altitude"=>"35.0", "accuracy"=>"3072.0", "longitude"=>longitude}}
     post :create, record
     Location.count.should == count+1
     location = Location.last
     location.user.openidentities.any?{|o| o.url == identity}.should be_true
     location.timestamp.should == Time.parse("2009-01-09T18:43:56+0000")
+    location.geom.y.should == latitude
+    location.geom.x.should == longitude
   end
 
   it "should display a user's location summary" do
