632840d06f25f19d98e09ed04149731510a36b39
Don Park <don@donpark.org>
2009-02-03 11:10:26 -0800

    throw in some lambda, change() action. booya!

--- spec/controllers/location_controller_spec.rb
+++ spec/controllers/location_controller_spec.rb
@@ -3,7 +3,6 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe LocationsController do
   it "should process a 2008-style location update" do
     identity = "http://testperson/"
-    count = Location.count
     latitude = 45.5118191242218
     longitude = -122.63253271579742
     record = {"client"=>{"version"=>"20081227"}, "location"=>{"timestamp"=>"2009-01-09T18:43:56+0000", "latitude"=>latitude, "guid"=>identity, "altitude"=>"35.0", "accuracy"=>"3072.0", "longitude"=>longitude}}
@@ -12,8 +11,9 @@ describe LocationsController do
     openid.should_receive(:user).and_return(user)
     Openidentity.should_receive(:lookup_or_create).with(identity).and_return(openid)
 
-    post :create, record
-    Location.count.should == count+1
+    lambda do
+      post :create, record
+    end.should change(Location, :count).by(1)
     location = Location.last
     location.timestamp.should == Time.parse("2009-01-09T18:43:56+0000")
     location.geom.y.should == latitude
@@ -31,7 +31,9 @@ describe LocationsController do
     token.should_receive(:user).and_return(user)
     OauthToken.should_receive(:find_by_token).with(token_string).and_return(token)
 
-    post :create, record
+    lambda do
+      post :create, record
+    end.should change(Location, :count).by(1)
   end
 
   it "should display a user's location summary" do
