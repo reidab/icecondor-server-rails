a7eb7795d84a9d4020c9085354090c136f92ee11
Don Park <don@donpark.org>
2009-02-07 20:27:10 -0800

    make all user status private by default

--- db/migrate/20090208030246_add_status_to_users.rb
+++ db/migrate/20090208030246_add_status_to_users.rb
@@ -1,6 +1,9 @@
 class AddStatusToUsers < ActiveRecord::Migration
   def self.up
    add_column :users, :access_status, :string
+   User.all.each do |user|
+     user.update_attribute :access_status, "private"
+   end
   end
 
   def self.down
