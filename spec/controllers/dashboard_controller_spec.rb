8a3f7498aa077c7164159babc9182479eaa46b8c
Don Park <don@donpark.org>
2009-02-08 10:47:48 -0800

    check response on the dashboard front page

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe DashboardController do
   it "should display the front page" do
     get :index
-    respose.should be_success
+    response.should be_success
   end
 end
