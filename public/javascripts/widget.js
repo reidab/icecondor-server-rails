1d53426d175e8cdf633e9044f35007fc7c719c0e
Don Park <don.park@gmail.com>
2010-03-25 17:11:57 -0700

    javascript fixes

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
   function pollIcecondor(id) {
     $('icecondor').innerHTML = "loading last IceCondor update.";
-    new Ajax.Request('/locations.json?id='+id, {
+    new Ajax.Request('http://www.icecondor.com/locations.json?id='+id, {
       method: 'get',
       onSuccess: function(response) {
         $('icecondor').innerHTML = "last checkin: "+response.responseJSON[0].location['timestamp'];
       }
     });
   }
-</script>
 
 
