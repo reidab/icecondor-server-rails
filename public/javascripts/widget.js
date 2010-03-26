f8786319bac813cf477d7372ea65e2b998aaa477
Don Park <don.park@gmail.com>
2010-03-25 17:17:03 -0700

    last of the Prototype version

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,7 +1,10 @@
   function pollIcecondor(id) {
-    $('icecondor').innerHTML = "loading last IceCondor update.";
+    $('icecondor').innerHTML = "IceCondor goes here.";
     new Ajax.Request('http://www.icecondor.com/locations.json?id='+id, {
       method: 'get',
+      onLoading: function(response) {
+        $('icecondor').innerHTML = "loading last IceCondor update.";
+      },
       onSuccess: function(response) {
         $('icecondor').innerHTML = "last checkin: "+response.responseJSON[0].location['timestamp'];
       }
