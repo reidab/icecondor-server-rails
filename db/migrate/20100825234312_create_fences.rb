4d48f83fbe97a20e9569aaf338e91fa85f51ae56
Don Park <don@donpark.org>
2010-08-25 17:08:18 -0700

    fences belong to a user

--- db/migrate/20100825234312_create_fences.rb
+++ db/migrate/20100825234312_create_fences.rb
@@ -2,6 +2,7 @@ class CreateFences < ActiveRecord::Migration
   def self.up
     create_table "fences" do |t|
       t.column "geom", :polygon, :null=>false, :srid => 4326, :with_z => true
+      t.integer :user_id
       t.timestamps
     end
 
