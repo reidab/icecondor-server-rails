41bdaeec16c2e1855bb3ff76b2568d21b95aded1
Don Park <don@donpark.org>
2010-09-17 15:31:18 -0700

    Save a geofence

--- app/controllers/fences_controller.rb
+++ app/controllers/fences_controller.rb
@@ -6,6 +6,13 @@ class FencesController < ApplicationController
   end
 
   def create
-    render :text => params.inspect
+    points = params[:points].map{|sp| sp.split(',').map{|sp|sp.to_f}}
+    logger.info(points.inspect)
+    # close the ring
+    points << points.first
+    wgs84_srid = 4326
+    current_user.fences.create(:name => params[:fence][:name],
+                               :geom => Polygon.from_coordinates([points], wgs84_srid, true))
+    redirect_to :controller => :users, :action => :show, :id => current_user.username
   end
 end
