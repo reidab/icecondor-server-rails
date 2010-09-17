edf846dab04f0b1e0229bff5822cad73811ec3fa
Don Park <don@donpark.org>
2010-09-17 14:59:25 -0700

    define a new fence

--- app/controllers/fences_controller.rb
+++ app/controllers/fences_controller.rb
@@ -1,2 +1,11 @@
 class FencesController < ApplicationController
+  def new
+    respond_to do |wants|
+      wants.html { }
+    end
+  end
+
+  def create
+    render :text => params.inspect
+  end
 end
