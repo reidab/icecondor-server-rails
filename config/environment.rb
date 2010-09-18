ba0f2b03710027fced3a07f6ba94d4bfaa9afdf8
Don Park <don@donpark.org>
2010-09-18 12:52:10 -0700

    grr. ruby problems. says rspec 1.3.0 is not there when it is

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rfc-822"
   config.gem "redfinger"
-  config.gem "rspec", :version => "1.3.0"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
