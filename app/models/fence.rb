130fd4a229cf7f6496021a1a741dedd43b2f6832
Don Park <don@donpark.org>
2010-09-18 09:24:18 -0700

    POSTGIS specific. quickly outgrew georuby.

--- app/models/fence.rb
+++ app/models/fence.rb
@@ -1,3 +1,18 @@
 class Fence < ActiveRecord::Base
   belongs_to :user
+
+  def area_in_sq_ft
+    area = ActiveRecord::Base.connection.execute("select ST_Area(geom) from fences where id=#{id}")
+    area[0]["st_area"].to_f
+  end
+
+  def count_points
+    area = ActiveRecord::Base.connection.execute("select ST_NPoints(geom) from fences where id=#{id}")
+    area[0]["st_npoints"].to_i
+  end
+
+  def contains?(location)
+    area = ActiveRecord::Base.connection.execute("select ST_Intersects(ST_GeomFromEWKT('#{location.geom.as_ewkt}',geom) from fences where id=#{id}")
+    area[0]["st_intersects"]=='t'
+  end
 end
