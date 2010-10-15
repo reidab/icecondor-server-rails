9bbf95aa79b1c15882ad1701ae6c8e2f2ed71882
Don Park <don@donpark.org>
2010-10-15 15:37:14 -0700

    omg refactor alert

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -13,6 +13,6 @@ class UserMailer < ActionMailer::Base
       p.body = render_message("trigger_email", :trigger => trigger, :inout => inout, :location => location)  
     end 
 
-    attachment :content_type => "image/jpeg",  :body => HTTParty.get("http://maps.google.com/maps/api/staticmap?center=#{location.latitude},#{location.longitude}&zoom=14&size=256x256&sensor=false&markers=color:green%7Clabel:X%7C#{location.latitude},#{location.longitude}"), :content_location => "map.jpg"
+    attachment :content_type => "image/jpeg",  :body => HTTParty.get("http://maps.google.com/maps/api/staticmap?center=#{location.latitude},#{location.longitude}&zoom=14&size=256x256&sensor=false&markers=color:green%7Clabel:X%7C#{location.latitude},#{location.longitude}&path=color:0x00000000%7Cweight:5%7Cfillcolor:0xFFFF0033%7C#{trigger.fence.geom.rings.first.points.map{|p|"#{p.y},#{p.x}"}.join("%7C")}"), :content_location => "map.jpg"
   end  
 end
