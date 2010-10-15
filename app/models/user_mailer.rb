10053754730b127a8d76cbc7e1a977181adaab31
Don Park <don@donpark.org>
2010-10-15 12:53:27 -0700

    add a marker to the attached map

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -13,6 +13,6 @@ class UserMailer < ActionMailer::Base
       p.body = render_message("trigger_email", :trigger => trigger, :inout => inout, :location => location)  
     end 
 
-    attachment :content_type => "image/jpeg",  :body => HTTParty.get("http://maps.google.com/maps/api/staticmap?center=#{location.latitude},#{location.longitude}&zoom=14&size=256x256&sensor=false"), :content_location => "map.jpg"
+    attachment :content_type => "image/jpeg",  :body => HTTParty.get("http://maps.google.com/maps/api/staticmap?center=#{location.latitude},#{location.longitude}&zoom=14&size=256x256&sensor=false&markers=color:green|label:X|#{location.latitude},#{location.longitude}"), :content_location => "map.jpg"
   end  
 end
