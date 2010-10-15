f8143db4d0956da7f32c925aa797ab720a58fa06
Don Park <don@donpark.org>
2010-10-15 12:29:36 -0700

    go back to multipart alternative

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
     recipients email 
     from "IceCondor Trigger <triggers@icecondor.com>"  
     subject "#{inout} #{trigger.fence.name}"  
+    content_type "multipart/alternative"
 
     part "text/html" do |p| 
       p.body = render_message("trigger_email", :trigger => trigger, :inout => inout, :location => location)  
