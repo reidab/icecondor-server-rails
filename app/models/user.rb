e629136ba1588bfd0b0a72354489f5c88dd07bac
Don Park <don@donpark.org>
2010-11-13 12:03:23 -0800

    clean up dependent records

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
 
-  has_many :openidentities
-  has_many :client_applications
-  has_many :tokens, :class_name=>"OauthToken",:order=>"authorized_at desc",:include=>[:client_application]
-  has_many :locations
-  has_many :fences
-  has_many :triggers
+  has_many :openidentities, :dependent => :destroy
+  has_many :client_applications, :dependent => :destroy
+  has_many :tokens, :class_name=>"OauthToken",
+                    :order=>"authorized_at desc",
+                    :include=>[:client_application],
+                    :dependent => :destroy
+  has_many :locations, :dependent => :destroy
+  has_many :fences, :dependent => :destroy
+  has_many :triggers, :dependent => :destroy
 
   before_create :set_defaults
 
@@ -45,5 +48,8 @@ class User < ActiveRecord::Base
 
   def blur?
     inside_fences.any?{|fence| fence.triggers.any?{|trigger| trigger.action == "blur"}} 
+  end
+
+  def webfinger_local_address
   end
 end
