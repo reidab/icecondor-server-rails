b22bd58f694eb0ee0e2ab8115096f8aa1abd52da
Don Park <don@donpark.org>
2011-01-28 16:57:51 -0800

    the start of webfinger friending

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,8 +4,26 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    current_user.friends << User.find(params[:friendship][:friend_id])
-    redirect_to :controller => :users,
+    username, domain = params[:email].split('@')
+    case domain
+    when "icecondor.com"
+      friend = User.find_by_username(username)
+    else
+      begin
+        finger = Redfinger.finger(params[:email])
+      rescue Redfinger::ResourceNotFound => e
+        flash[:error] = "webfinger err: #{e}"
+      rescue SocketError => e
+        flash[:error] = "#{identifier}: #{e}"
+      end
+    end
+
+    if friend
+      current_user.friends << friend
+      redirect_to :controller => :users,
                 :action => :show, :id => current_user.username
+    else
+      redirect_to :action => :new
+    end
   end
 end
