41baf7e5ab3633c506cb568502acf8ddc38621b0
Don Park <don@donpark.org>
2011-01-28 16:48:52 -0800

    friend existing users

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,11 @@
 class FriendshipsController < ApplicationController
-  
+  def new
+    @friendship = Friendship.new
+  end
+
+  def create
+    current_user.friends << User.find(params[:friendship][:friend_id])
+    redirect_to :controller => :users,
+                :action => :show, :id => current_user.username
+  end
 end
