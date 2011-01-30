991470295926e9cacbaac2ed54ea9e013d7d40a6
Don Park <don@donpark.org>
2011-01-29 18:15:55 -0800

    webfinger yeah

--- app/controllers/webfinger_controller.rb
+++ app/controllers/webfinger_controller.rb
@@ -2,7 +2,7 @@ class WebfingerController < ApplicationController
   layout nil
 
   def xrd
-    @user = User.find_by_username(params[:acct])
+    @user = User.find_by_username(params[:acct].split('@').first)
     render :status => 404, :nothing => true unless @user
   end
 end
